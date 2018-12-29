fa69e6da1a389fefdf6365f070cb2e70f2f4f7f3
t532 <2037246484@qq.com>
2018-12-30 00:31:08 +0800

    add type definition for class Command

---
+++ src/command/definition.ts
@@ -0,0 +1,26 @@
+/** A list of delcared parameters */
+export interface IParameters {
+    /** The keys are the aliases and the values are param names  */
+    aliases: any
+    /** The keys are param names and the values are default values */
+    defaults: any
+    /** An array of ordered params */
+    ordered: string[]
+    /** An array of required params */
+    required: string[]
+    /** Description of parameters */
+    description: any
+}
+
+/** A list of given arguments */
+export interface IArguments {
+    /** The specified options */
+    options: any[]
+    /** The specified arguments */
+    arguments: any
+    /** Rest unparsed items */
+    rest: any[]
+}
+
+/** An extraneous processor for arguments */
+export type TExtraneousProcessor = (args: IArguments, params: { parameters: IParameters, options: string[] }) => IArguments
--- src/command/index.ts
+++ src/command/index.ts
@@ -1,1 +1,2 @@
 export { Command } from './command'
+export { IParameters, IArguments, TExtraneousProcessor } from './definition'
