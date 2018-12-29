93dd780243c9fd85877f7561d66cb12c2e0804c3
t532 <2037246484@qq.com>
2018-12-30 01:04:36 +0800

    add comments to class Middlware, fix its scope bugs

--- src/middleware/manager.ts
+++ src/middleware/manager.ts
@@ -1,14 +1,24 @@
 import { generateNextExecutor } from './next'
+/** A middleware manager */
 export class MiddlewareManager {
+    /** The list of middlewares */
     middlewares: ((ctx: any, next: () => Promise<void>) => void)[] = []
+    /**
+     * add a middleware to the middleware list
+     * @param middleware the middleware
+     */
     use(middleware: (ctx: any, next: () => Promise<void>) => void) {
         this.middlewares = [...(this.middlewares || []), middleware]
         return this
     }
+    /**
+     * Let context go through the middlewares
+     * @param ctx the context
+     */
     async run(ctx: any) {
         const executed = this.middlewares.map(() => false)
         for (let mw in this.middlewares)
             if (!executed[mw])
-                await this.middlewares[mw](ctx, generateNextExecutor(parseInt(mw) + 1, ctx, executed))
+                await this.middlewares[mw](ctx, generateNextExecutor(this.middlewares, parseInt(mw) + 1, ctx, executed))
     }
 }
--- src/middleware/next.ts
+++ src/middleware/next.ts
@@ -1,9 +1,18 @@
-export function generateNextExecutor(offset: number, context: any, recorder: boolean[]) {
+/**
+ * Generate a function that calls a function in an array, 
+ * records the call and pass a funciton that calls the next function, etc. recursively 
+ * until the end of the funciton array
+ * @param list the function array
+ * @param offset the position of the next function
+ * @param context the context that'll be passed to the function
+ * @param recorder the array that records whether a function in the function array is already called
+ */
+export function generateNextExecutor(list: ((ctx: any, next: any) => void)[], offset: number, context: any, recorder: boolean[]) {
     async function executeNext() {
-        if (this.middlewares[offset] instanceof Function && !recorder[offset]) {
+        if (list[offset] instanceof Function && !recorder[offset]) {
             recorder[offset] = true
-            await this.middlewares[offset](context, this._generateNextExecutor(offset + 1, context, recorder))
+            await list[offset](context, generateNextExecutor(list, offset + 1, context, recorder))
         }
     }
-    return executeNext.bind(this)
+    return executeNext
 }
