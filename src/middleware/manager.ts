f14bc5e7e117af639d4c93abacf5130385b00982
t532 <2037246484@qq.com>
2018-12-30 01:20:05 +0800

    add debug loggers for class MiddlewareManager

--- src/middleware/manager.ts
+++ src/middleware/manager.ts
@@ -1,4 +1,9 @@
 import { generateNextExecutor } from './next'
+import Debug from 'debug'
+const debug = {
+    use: Debug('ionjs: MiddlewareManager: use'),
+    run: Debug('verbose-ionjs: MiddlewareManager: run'),
+}
 /** A middleware manager */
 export class MiddlewareManager {
     /** The list of middlewares */
@@ -9,6 +14,7 @@ export class MiddlewareManager {
      */
     use(middleware: (ctx: any, next: () => Promise<void>) => void) {
         this.middlewares = [...(this.middlewares || []), middleware]
+        debug.use(`add new middleware, current size ${this.middlewares.length}`)
         return this
     }
     /**
@@ -16,9 +22,11 @@ export class MiddlewareManager {
      * @param ctx the context
      */
     async run(ctx: any) {
+        debug.run('start (ctx %o)', ctx)
         const executed = this.middlewares.map(() => false)
         for (let mw in this.middlewares)
             if (!executed[mw])
                 await this.middlewares[mw](ctx, generateNextExecutor(this.middlewares, parseInt(mw) + 1, ctx, executed))
+        debug.run('end (ctx %o)', ctx)
     }
 }
