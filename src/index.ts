b5eb6511a7e077526780dd858e7a8b4bb7add89c
t532 <2037246484@qq.com>
2018-12-30 05:34:33 +0800

    add part of the framework: init(), start() and useMiddleware()

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import * as Adapter from './adapter'
+import * as Command from './command'
+import * as Middleware from './middleware'
+import * as Session from './session'
+
+const _middleware = new Middleware.MiddlewareManager()
+
+function _defaultIdentifier(ctx) { return `${ctx.user_id}${ctx.message_type[0]}${ctx._union_id}` }
+const _session: any = { default: { single: new Session.SingleSessionManager(_defaultIdentifier), concurrent: new Session.ConcurrentSessionManager(_defaultIdentifier) } }
+
+const _config: { operators?: number[], prefixes?: string[], self?: number } = {}
+
+let _receivePort: number
+export let receiver: Adapter.Receiver
+
+export let sender: Adapter.Sender
+
+export function init({ receivePort = 8080, receiveSecret, sendURL = 'http://127.0.0.1:5700', sendToken, operators = [], prefixes = [], self }: {
+    receivePort: number, 
+    receiveSecret?: string, 
+    sendURL: string, 
+    sendToken?: string, 
+    operators?: number[],
+    prefixes?: string[],
+    self: number,
+}) {
+    _receivePort = receivePort
+    receiver = new Adapter.Receiver(receiveSecret)
+    sender = new Adapter.Sender(sendURL, sendToken)
+    _config.operators = operators
+    _config.prefixes = prefixes
+    _config.self = self
+    receiver.on('message', async ctx => {
+        await _middleware.run(ctx)
+        for (const i in _session) {
+            _session[i].single.run(ctx)
+            _session[i].multi.run(ctx)
+        }
+    })
+}
+
+export function start() { receiver.listen(this._receivePort) }
+
+export function useMiddleware(middleware: Middleware.TMiddleware) { _middleware.use(middleware) }
+
+// TODO: function useSession()
+
+export const Classes = { Adapter, Command, Middleware, Session }
