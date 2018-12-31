0050e0e5196f9ef99bd8aace1fe81ed6729ee202
t532 <2037246484@qq.com>
2019-01-01 04:30:01 +0800

    fix bugs in secret checking

--- src/adapter/receiver.ts
+++ src/adapter/receiver.ts
@@ -1,25 +1,33 @@
-import Koa = require('koa')
-import koaBody = require('koa-body')
+import * as Koa from 'koa'
+import * as koaBody from 'koa-bodyparser'
 import Debug from 'debug'
 import { createHmac } from 'crypto'
 import { EventEmitter } from 'events'
+declare module 'koa' {
+    interface Request {
+        body: any;
+        rawBody: string;
+    }
+}
+
 const debug = Debug('ionjs: Receiver')
 export class Receiver extends EventEmitter {
     private readonly _server = new Koa()
     constructor(secret?: string) {
         super()
         const thisRef = this
+        this._server.use(koaBody())
         if (secret) {
-            this._server.use(async ctx => {
+            this._server.use(async (ctx, next) => {
                 ctx.assert(ctx.request.headers['x-signature'] !== undefined, 401);
-                const sig = createHmac('sha1', secret).update((ctx.request as Koa.Request&{ rawBody: string }).rawBody).digest('hex')
+                const sig = createHmac('sha1', secret).update(ctx.request.rawBody).digest('hex')
                 ctx.assert(ctx.request.headers['x-signature'] === `sha1=${sig}`, 403)
+                await next()
             })
         }
-        this._server.use(koaBody())
         this._server.use(async ctx => {
             const msg = ctx.request.body
-            debug(`received: ${JSON.stringify(msg)}`)
+            debug(`received: %o`, msg)
             let events: string[] = ['post', msg.post_type, `${msg.post_type}/${msg.message_type || msg.request_type || msg.notice_type || msg.meta_event_type}`]
             msg._union_id = msg.group_id || msg.discuss_id || msg.user_id
             if (msg.sub_type) events.push(`${msg.post_type}/${msg.message_type || msg.request_type || msg.notice_type || msg.meta_event_type}/${msg.sub_type}`)
