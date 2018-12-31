1cb340cf1282257b12fb75174d5a6f087777dd6b
t532 <2037246484@qq.com>
2019-01-01 04:40:13 +0800

    let use() accept more than one function

--- src/middleware-manager.ts
+++ src/middleware-manager.ts
@@ -2,6 +2,9 @@ import { MiddlewareManager, TMiddleware } from './middleware'
 
 const manager = new MiddlewareManager()
 
-export function use(middleware: TMiddleware) { manager.use(middleware) }
+export function use(...middlewares: TMiddleware[]) { 
+    for (const mw of middlewares)
+        manager.use(mw) 
+}
 
 export function run(ctx: any) { return manager.run(ctx) }
