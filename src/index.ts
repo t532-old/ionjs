fc231ca3137ec9f8b6d98ccdd0773d5b18906222
t532 <2037246484@qq.com>
2019-01-01 04:40:35 +0800

    delete useless variable, export classes directly

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import * as Adapter from './adapter'
-import * as Command from './command'
-import * as Middleware from './middleware'
-import * as Session from './session'
-
 import { init as initWhen, When } from './when'
 import { init as initSender, sender } from './sender'
 import { init as initReceiver, start, receiver } from './receiver'
 import { use as useSession, run as runSession } from './session-managers'
 import { use as useMiddleware, run as runMiddleware } from './middleware-manager'
-
-const _config: { operators?: number[], prefixes?: string[], self?: number } = {}
 
 export function init({ receivePort = 8080, receiveSecret, sendURL = 'http://127.0.0.1:5700', sendToken, operators = [], prefixes = [], self }: {
     receivePort: number, 
@@ -33,4 +26,7 @@ export { start }
 export { sender, receiver }
 export { runMiddleware, runSession }
 export { useMiddleware, useSession }
-export const Classes = { Adapter, Command, Middleware, Session }
+export * from './adapter'
+export * from './command'
+export * from './middleware'
+export * from './session'
