1e35f4451ad49efc3348042fbf6d153ec02dc5e2
t532 <2037246484@qq.com>
2019-01-23 21:39:46 +0800

    refactor When with validator callbacks

--- src/classes/when/base.ts
+++ src/classes/when/base.ts
@@ -1,4 +1,4 @@
-import { TValidator, TParser, TWhenClass } from './definitions'
+import { TValidator, TValidatorCallback, TParser, TWhenClass } from './definitions'
 
 /** A class that represents a series of conditions */
 export class When {
@@ -6,29 +6,41 @@ export class When {
     protected readonly _validators: TValidator[] = []
     /** The parsers */
     protected readonly _parsers: TParser[] = []
+    /** The validator callback (when valid) */
+    protected readonly _validCallbacks: TValidatorCallback[] = []
+    /** The validator callback (when invalid) */
+    protected readonly _invalidCallbacks: TValidatorCallback[] = []
     /**
      * @param validators A list of validators
      * @param parsers A list of parsers
      */
-    constructor(validators: TValidator[], parsers: TParser[]) {
-        this._validators = validators
-        this._parsers = parsers
+    constructor({ validate = [], parse = [], validCallback = [], invalidCallback = [] }: {
+        validate?: TValidator[], 
+        parse?: TParser[],
+        validCallback?: TValidatorCallback[],
+        invalidCallback?: TValidatorCallback[],
+    } = {}) {
+        this._validators = validate
+        this._parsers = parse
+        this._validCallbacks = validCallback
+        this._invalidCallbacks = invalidCallback
     }
     /** Returns a new When instance based on this, with one more validator and/or parser */
-    protected deriveFromType<T extends When>({ validate, parse }: { validate?: TValidator, parse?: TParser }) {
-        const validators = Array.from(this._validators),
-              parsers = Array.from(this._parsers)
-        if (validate) {
-            const last = validators.findIndex(fn => fn.name === validate.name)
-            if (last >= 0) validators.splice(last, 1)
-            validators.push(validate)
+    protected deriveFromType<T extends When>(deriviation: { validate?: TValidator, parse?: TParser, validCallback?: TValidatorCallback, invalidCallback?: TValidatorCallback }) {
+        const original = {
+            validate: Array.from(this._validators),
+            parse: Array.from(this._parsers),
+            valid: Array.from(this._validCallbacks),
+            invalid: Array.from(this._invalidCallbacks),
         }
-        if (parse) {
-            const last = parsers.findIndex(fn => fn.name === parse.name)
-            if (last >= 0) parsers.splice(last, 1)
-            parsers.push(parse)
+        for (const name in original) {
+            if (deriviation[name]) {
+                const last = original[name].findIndex(fn => fn.name === deriviation[name].name)
+                if (last >= 0) original[name].splice(last, 1)
+                original[name].push(deriviation[name])
+            }
         }
-        return new (this.constructor as TWhenClass<T>)(validators, parsers)
+        return new (this.constructor as TWhenClass<T>)(original)
     }
     /** Validate a context */
     async validate(ctx: any, ...extraArgs: any[]) {
--- src/classes/when/definitions.ts
+++ src/classes/when/definitions.ts
@@ -1,8 +1,18 @@
 /** A type alias for a validator function */
 export type TValidator = (ctx: any, ...extraArgs: any[]) => boolean|Promise<boolean>
 
+/** A type alias for a validator callback function */
+export type TValidatorCallback = (ctx: any, ...extraArgs: any[]) => void
+
 /** A type alias for a parser function */
 export type TParser = (ctx: any, ...extraArgs: any[]) => any|Promise<any>
 
 /** A type alias for class When and its derived classes */
-export type TWhenClass<T> = Function&{ new(validators: TValidator[], parsers: TParser[]): T }
+export type TWhenClass<T> = Function&{ 
+    new({ validate, parse, validCallback, invalidCallback }: {
+        validate?: TValidator[], 
+        parse?: TParser[],
+        validCallback?: TValidatorCallback[],
+        invalidCallback?: TValidatorCallback[],
+    }): T 
+}
