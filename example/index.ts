117369163d911df2bc9bebdcb1ad62b9b1d55af8
t532 <2037246484@qq.com>
2019-02-06 15:22:15 +0800

    fix bugs in example

--- example/index.ts
+++ example/index.ts
@@ -1,21 +1,22 @@
-import { init, start, sender, useModuleMetadata, useRegistrationMetadata } from '@ionjs/core'
+import { init, start, sender } from '@ionjs/core'
 
 init({
     receivePort: 8080,
     sendURL: 'http://localhost:5700',
-    self: 1145141919,
+    self: 114514893,
+    operators: [1919810],
     prefixes: ['!', '！', '/', '／', '']
 })
 
 setInterval(async () => {
-    try { await sender.to({ message_type: 'private', user_id: 1002647525 }).send('哇') }
+    try { await sender.to({ message_type: 'private', user_id: 1919810 }).send('哇') }
     catch (err) { console.error(err) }
 }, 10 * 1000)
 
-require('./plugins/timeout')
-require('./plugins/usage')
-require('./plugins/tuling')
-require('./plugins/repeater')
-require('./plugins/weather')
+require('./modules/timeout')
+require('./modules/usage')
+require('./modules/tuling')
+require('./modules/repeater')
+require('./modules/weather')
 
 start()
