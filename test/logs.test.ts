714321140f677553b5ed9c7cf280dcc2d5105c51
t532 <2037246484@qq.com>
2019-02-11 13:10:35 +0800

    test(logs): use a more significant timeout (3000ms) for middlewareTimeout
    If we use a shorter time (e.g. a couple of hundreds ms) the test result will depend on the
    environment, so a more significant timeout is needed.

--- test/logs.test.ts
+++ test/logs.test.ts
@@ -1,15 +1,16 @@
 import * as ionjs from '../src'
 import axios from 'axios'
 const PORT_OFFSET = 4
+function wasteTime(time = 100) { return new Promise(resolve => setTimeout(resolve, time)) }
 
 ionjs.init({
     receivePort: 8080 + PORT_OFFSET,
     sendURL: `http://localhost:${5700 + PORT_OFFSET}`,
     self: 114514,
-    middlewareTimeout: 400,
+    middlewareTimeout: 3000,
 })
 ionjs.useMiddleware(async (ctx, next) => {
-    if (ctx.message === 'mw-timeout') setTimeout(next, 500)
+    if (ctx.message === 'mw-timeout') await wasteTime(4000)
     else await next()
 })
 ionjs.useMiddleware(async (ctx, next) => {
@@ -25,7 +26,7 @@ ionjs.start()
 
 test('Middleware timeout', async (done) => {
     await axios.post(`http://localhost:${8080 + PORT_OFFSET}`, { message: 'mw-timeout' })
-    setTimeout(done, 600)
+    setTimeout(done, 4000)
 })
 
 test('Session error', async (done) => {
