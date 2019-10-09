6dbe34eacdd48a7f812a2c14db0e6f3c9214192a
t532 <2037246484@qq.com>
2019-10-09 22:29:37 +0800

    fix(unionIdOf): message_type now overrides the original logic (if exists)
    If a message object has message_type, unionIdOf will determine union id by this field's value.

--- src/platform/receiver/util.ts
+++ src/platform/receiver/util.ts
@@ -2,10 +2,14 @@ import { IMessage } from './definition'
 
 export function contextTypeOf(ctx: IMessage) {
     const events: string[] = ['post', ctx.post_type, `${ctx.post_type}/${ctx.message_type || ctx.request_type || ctx.notice_type || ctx.meta_event_type}`]
-    if (ctx.sub_type) events.push(`${ctx.post_type}/${ctx.message_type || ctx.request_type || ctx.notice_type || ctx.meta_event_type}/${ctx.sub_type}`)
+    if (ctx.sub_type)
+        events.push(`${ctx.post_type}/${ctx.message_type || ctx.request_type || ctx.notice_type || ctx.meta_event_type}/${ctx.sub_type}`)
     return events
 }
 
 export function unionIdOf(ctx: IMessage): number {
-    return ctx.discuss_id || ctx.group_id || ctx.user_id
+    if (ctx.message_type)
+        return ctx[`${ctx.message_type === 'private' ? 'user' : ctx.message_type}_id`]
+    else 
+        return ctx.discuss_id || ctx.group_id || ctx.user_id
 }
